6e19f6cc3529c1a8dc6a59dfd961ff0226f5ea0d
Kessy <u.kesandu@yahoo.com>
2024-06-22 08:28:03 +0100

    chore: forge init

--- script/DeploySimpleStorage.s.sol
+++ script/DeploySimpleStorage.s.sol
@@ -5,10 +5,11 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {SimpleStorage} from "../src/SimpleStorage.sol";
 
-contract DeploySimpleStorage is script {
+contract DeploySimpleStorage is Script {
     function run() external returns (SimpleStorage) {
         vm.startBroadcast();
-        SimpleStorage simplestorage = new SimpleStorage;
+        SimpleStorage simplestorage = new SimpleStorage();
         vm.stopBroadcast();
+        return simplestorage;
     }
 }
